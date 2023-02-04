1e4423de0d9bf838da09692811d4a83e1ded6d5b
BHASKAR RANJAN BORA <bhaskarranjan44@gmail.com>
2023-02-04 23:57:39 +0530

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,6 +1,5 @@
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -13,6 +12,10 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   def correct_name
